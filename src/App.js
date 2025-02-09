e82ff79d7282de23c16c03717a3fcee1b309bbbe
ChristopherL <anbchris@gmail.com>
2025-02-09 09:44:14 -0500

    Optimizing code by creating directory component and home component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,21 @@
-import Directory from './components/directory/directory.component';
+import { Routes, Route } from 'react-router';
 
+import Home from './routes/home.component.jsx';
 
-const App = () => { 
-  const categories = [
-    {
-      "id": 1,
-      "title": "hats",
-      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
-    },
-    {
-      "id": 2,
-      "title": "jackets",
-      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
-    },
-    {
-      "id": 3,
-      "title": "sneakers",
-      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
-    },
-    {
-      "id": 4,
-      "title": "womens",
-      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
-    },
-    {
-      "id": 5,
-      "title": "mens",
-      "imageUrl": "https://media.gettyimages.com/id/172909725/photo/the-aviator.jpg?s=612x612&w=0&k=20&c=UlwzIP5vr56cfODK3epvRLGwsjag8JWwOgONvsKh3qc="
-    }
-  ]
-
+const Shop =  () => {
   return (
-    <Directory categories={categories} />
-  );
+    <h1>I am the shop page</h1>
+  )
+}
+
+const App = () => {
+  return (
+  <Routes>
+    <Route path='/home' element={<Home />}>
+      <Route path='shop' element={<Shop />} />
+    </Route>
+  </Routes> 
+  )
 }
 
 export default App;
