af88b3ef12fc901e6341ca9c32d6f923ec734ad8
ChristopherL <anbchris@gmail.com>
2025-03-08 21:07:58 -0500

    implemented redux persist library to retaincart and category state

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 // import logger from 'redux-logger';
+
 import { rootReducer } from './root-reducer';
 
 const loggerMiddleware = (store) => (next) => (action) => {
@@ -15,9 +18,19 @@ const loggerMiddleware = (store) => (next) => (action) => {
     console.log('next state: ', store.getState());
 }
 
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user'] // our user is coming from firebase auth state listener.
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 // all middle wares need to be passed in the 3rd params as so:
-export const store = createStore(rootReducer, undefined, composedEnhancers); 
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
