dc9c524ee7d760b53467d4a187d630c5c3bc3832
ChristopherL <anbchris@gmail.com>
2025-03-10 18:37:09 -0400

    implemented redux thunk to return functions instead of objects for asynchronus logic and API calls

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,22 +1,32 @@
 import { Link } from 'react-router';
+import { useSelector } from 'react-redux';
+
+import { selectCategoriesIsLoading } from '../../store/categories/category.selector';
 
 import ProductCard from '../product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 import './category-preview.styles.scss';
 
 const CategoryPreview = ({ title, products }) => {
+    const isLoading = useSelector(selectCategoriesIsLoading);
+
     return (
         <div className='category-preview-container' >
             <h2>
                 <Link className='title' to={title} >{title.toUpperCase()}</Link>
             </h2>
-            <div className='preview' >
-                {products
-                    .filter((_, index) => index < 4)
-                    .map(product => (
-                        <ProductCard key={product.id} product={product} />
-                    ))}
-            </div>  
+            {isLoading ? (
+                <Spinner />
+            ) : (
+                <div className='preview' >
+                    {products
+                        .filter((_, index) => index < 4)
+                        .map(product => (
+                            <ProductCard key={product.id} product={product} />
+                        ))}
+                </div>  
+            )}
         </div>
     )
 }
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -3,14 +3,17 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router';
 
 import ProductCard from '../../components/product-card/product-card.component';
+import Spinner from '../../components/spinner/spinner.component';
 
 import { selectCategoriesMap } from '../../store/categories/category.selector';
+import { selectCategoriesIsLoading } from '../../store/categories/category.selector';
 
 import './category.styles.scss';
 
 const Category = () => {
     const { category } = useParams();
     const categoriesMap = useSelector(selectCategoriesMap);
+    const isLoading = useSelector(selectCategoriesIsLoading);
     const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
@@ -20,11 +23,15 @@ const Category = () => {
     return (
         <Fragment>
             <h2 className='category-title' >{category.toUpperCase()}</h2>
-            <div className='category-container'>
-                {products && products.map(product => {
-                    return <ProductCard key={product.id} product={product} />
-                })}
-            </div>
+            {isLoading ? (
+                <Spinner />
+            ) : (
+                <div className='category-container'>
+                    {products && products.map(product => {
+                        return <ProductCard key={product.id} product={product} />
+                    })}
+                </div>
+            )}
         </Fragment>
     )
 }
